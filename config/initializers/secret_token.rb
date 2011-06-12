bd9f941dcab8eed00b162318d610f89b66281c72
Bob Vargas <bobvar@hotmail.com>
2011-06-11 20:05:52 -0400

    Initial repo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scouts::Application.config.secret_token = '82494a643d34ad0f3912938bc2723b04812672944f06cb926e25026322bd66de7cacbb0d44ef84e3492bbee703d14eaba3a0ec18cc38e99d77ef988d99f3ad83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scouts::Application.config.session_store :cookie_store, :key => '_scouts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scouts::Application.config.session_store :active_record_store
